09f481c74cda66fb69d097c1d9ae210cba82c9cd
beccakosey22 <820512@mystma.org>
2021-09-28 09:18:03 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,36 @@
-#WRITE YOUR CODE IN THIS FILE
+#Create a function for each different comparison that can be made between two given variables x and y. The functions should be named:
+
+#creat fucntion greaterThan()
+def greaterThan(x, y):
+    if x > y:
+        return True
+    else:
+        return False
+
+#create function lessThan()
+def lessThan(x, y):
+    if x < y:
+        return True
+    else:
+        return False
+
+#create function equalTo()
+def equalTo(x, y):
+    if x == y:
+        return True
+    else:
+        return False
+
+#create greaterOrEqual(x, y)
+def greaterOrEqual(x, y):
+    if x <= y:
+        return True
+    else:
+        return False
+
+#create function lessOrEqual()
+def lessOrEqual(x, y):
+    if x <= y:
+        return True
+    else:
+        return False
